82ff26291b667c4d11df8845a84f47aaa3c57d43
David Cormell <d.cormell@etoncollege.org.uk>
2024-10-09 15:37:41 +0100

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Cormell, David - DPC on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
